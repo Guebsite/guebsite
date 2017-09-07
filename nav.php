a1f16a1b1245aabfd3893ae6936f1035d9e4020d
@ReynierNR <reynier.nr@gmail.com>
2017-09-06 19:46:11 -0700

    nav correction
    nav correction

--- nav.php
+++ nav.php
@@ -9,16 +9,14 @@
       <ul id="menu">
         <li><a href="<?php bloginfo('url') ?>" class="homebtn">Home</a></li>
         <li>
-            <a href="<?php bloginfo('url') ?>/sitiosweb/">Sitios Web &#8595;</a>
+            <a href="#">Sitios Web &#8595;</a>
             <ul class="hidden">
-                <li><a href="<?php bloginfo('url') ?>/sitiosweb/psd-html/">Psd a Html</a></li>
-                <li><a href="<?php bloginfo('url') ?>/sitiosweb/hazlo-tu-mismo/">Hazlo Tu Mismo FREE</a></li>
                 <li><a href="<?php bloginfo('url') ?>/sitiosweb/construimos-tu-sitio-web/">Lo hacemos x Ti</a></li>
                 <li><a href="<?php bloginfo('url') ?>/sitiosweb/sitio-web-wordpress/">Sitio Web Wordpress</a></li>
             </ul>
         </li>
         <li>
-            <a href="<?php bloginfo('url') ?>/marketing-online/">Marketing Online &#8595;</a>
+            <a href="#">Marketing Online &#8595;</a>
             <ul class="hidden">
                 <li><a href="<?php bloginfo('url') ?>/marketing-online/redes-sociales/">Redes Sociales</a></li>
                 <li><a href="<?php bloginfo('url') ?>/marketing-online/pago-por-clicks/">Mkt Pago por Click</a></li>
@@ -27,7 +25,7 @@
             </ul>
         </li>
         <li>
-            <a href="<?php bloginfo('url') ?>/servicios/">+ Servicios &#8595;</a>
+            <a href="#">+ Servicios &#8595;</a>
             <ul class="hidden">
                 <li><a href="<?php bloginfo('url') ?>/servicios/alojamiento-web/">Aojamiento Web</a></li>
                 
